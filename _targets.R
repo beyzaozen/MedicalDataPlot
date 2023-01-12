db6285cd77399c4ceffa3e74133a97e0938ac164
beyzaozen <78658759+beyzaozen@users.noreply.github.com>
2023-01-12 17:58:36 +0100

    fix error on saving image

--- _targets.R
+++ _targets.R
@@ -10,6 +10,6 @@ list(
   
   tar_target(insurance_plot, make_plot(insurance_data)),
   
-  tar_target(saved_plot, save_plot("fig/insurance_plot.png", insurance_plot), format = "file")
+  tar_target(saved_plot, save_plot("insurance_plot.png", insurance_plot), format = "file")
   
 )
